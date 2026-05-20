3eb564f3d15c076f0905401d3ca0df5bd465dae9
King-Wealth247 <wealthk91@gmail.com>
2026-05-20 18:21:08 +0100

    chore: initialize project with package.json and devDependencies for Gulp and Sass

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const {src, dest, watch, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function buildStyles() {
+    return src('scss/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(dest('css'));
+}
+
+function watchStyles() {
+    watch('scss/*.scss', buildStyles);
+}
+
+//exports.buildStyles = buildStyles;
+//exports.watchStyles = watchStyles;
+exports.default = series(buildStyles, watchStyles);
