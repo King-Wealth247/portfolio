f99a0062a703986876d55335febffb2aa74cff1f
King-Wealth247 <wealthk91@gmail.com>
2026-05-25 02:31:37 +0100

    Add language toggle functionality and update translations across multiple pages

---
+++ js/translation.js
@@ -0,0 +1,80 @@
+export const translation = {
+    en: {
+        //General
+        home: "Home",
+        about: "About",
+        projects: "Projects",
+        contact: "Contact",
+
+        //Home Page
+        greeting: "HI! My Name Is",
+        qualities: "Web Developer, Designer, And Programmer <br> <span class=\"aspiration-span\">Aspiring Machine Learning Engineer</span>",
+
+        //About Page
+        bio_intro: "My name is <span class=\"text-secondary\">KENGNI MONAME Holy Wealth</span>. I was born on October 1<sup>st</sup> 2006, in Cameroon, more precisely in the city of Douala. Since my tender age, I've aspired to be functionally part of the growing innovative world and thus, I engaged myself into a career path directed towards Information technology. I launched into <span class=\"text-secondary\">Computer Science Engineering (CSE)</span> and Developed skills related to Information Technologie<br>I am also a holder of a certification in Adobe Illustrator, MS Excel, and MySQL Database Administration.",
+        secondary_school_description: "Schooled at GBHS Nyalla - Douala from the years 2021 to 2024, over which I covered the classes Form 4 to UpperSixth and had both GCE Ordinary Level and Advanced Level.",
+        job1_title: "Intern at IT Department",
+        job1_description: "I did an internship at the IT Department of Pro-Pme Financement S.A. which is a financing company. I was tasked with handling little device malfunctionings and developing a web application with the aim of storing authorized personal information on the company staff members.",
+        job2_title: "Computer Science Engineering Student",
+        job2_description: "Presently Schooling at IUC, more precisely in the school of SEAS as a Barchelor of Technology (BTech) Student in the field of Computer Science Engineering. Aslo where I had the opportunity to have a couple of certifcations.",
+        skills_web_dev: "Web developement and related concepts, including the mastery of web programing languages like : HTML, CSS, SASS/SCSS, JavaScript, and PHP",
+        skills_full_stack: "Full stack developement; including mastery of programing languages like Python, React, React-Native, Next.js, Java, C, C++ and Visual Basics.<br><br><hr><br>Good understanding of Mobile android developement using XML and Java.",
+        skills_database: "Database Administration; with mastery of SQL",
+        skills_certifications: "Certified in Graphic design using Adobe Illustrator along side mastery of the use of various other graphic designing and video editing tools.<br><br><hr><br>Certified in MS Excel; with appropriate knowledge on the use of MS Office Suit.",
+        skills_drawing: "Highly skilled at pencil or digital drawing with average knowledge on animation",
+
+
+        //Projects Page
+        projects_title: "My Projects",
+        project1_desc: "Event Management System Using HTML, CSS, & JavaScript",
+        project2_desc: "Java Voting System",
+        project3_desc: "Copy of Amazone E-Shop, with JavaScript",
+        project4_desc: "SDLC-Model-Advisor",
+        project5_desc: "Internship Landing Page",
+        project6_desc: "Campus timetable Management and Mapping system",
+
+
+        //Contact Page
+        contact_me: "Contact Me"
+
+
+    },
+    fr: {
+        //General
+        home: "Accueil",
+        about: "À propos",
+        projects: "Projets",
+        contact: "Contact",
+
+        //Home Page
+        greeting: "BONJOUR! Je m'appelle",
+        qualities: "Développeur Web, Designer, Et Programmeur <br> <span class=\"aspiration-span\" data-lang=\"aspirations\">Ingénieur en Apprentissage Automatique en Herbe</span>",
+
+        
+        //About Page
+        bio_intro: "Je m'appelle <span class=\"text-secondary\">KENGNI MONAME Holy Wealth</span>. Je suis né le 1<sup>er</sup> octobre 2006, au Cameroun, plus précisément dans la ville de Douala. Depuis mon plus jeune âge, j'aspire à faire partie du monde innovant en pleine croissance et ainsi, je me suis engagé dans une carrière orientée vers les technologies de l'information. Je me suis lancé dans <span class=\"text-secondary\">l'ingénierie informatique (CSE)</span> et j'ai développé des compétences liées aux technologies de l'information.<br> Je suis également titulaire d'une certification en Adobe Illustrator, MS Excel et MySQL Database Administration.",
+        secondary_school_description: "Scolarisé à GBHS Nyalla - Douala de 2021 à 2024, au cours desquels j'ai couvert les classes de Form 4 à UpperSixth et obtenu à la fois le GCE Ordinary Level et le Advanced Level.",
+        job1_title: "Stagiaire au Département Informatique",
+        job1_description: "J'ai effectué un stage au département informatique de Pro-Pme Financement S.A., qui est une société de financement. J'étais chargé de gérer les petits dysfonctionnements des appareils et de développer une application web dans le but de stocker les informations personnelles autorisées sur les membres du personnel de l'entreprise.",
+        job2_title: "Étudiant en Génie Informatique",
+        job2_description: "Actuellement scolarisé à l'IUC, plus précisément à l'école de SEAS en tant qu'étudiant en Bachelor of Technology (BTech) dans le domaine du génie informatique. C'est aussi là que j'ai eu l'opportunité d'obtenir quelques certifications.",
+        skills_web_dev: "Développement web et concepts connexes, y compris la maîtrise des langages de programmation web tels que : HTML, CSS, SASS/SCSS, JavaScript et PHP",
+        skills_full_stack: "Développement full stack; y compris la maîtrise de langages de programmation tels que Python, React, React-Native, Next.js, Java, C, C++ et Visual Basics.<br><br><hr><br>Bonne compréhension du développement mobile Android utilisant XML et Java.", 
+        skills_database: "Administration de bases de données; avec maîtrise de SQL",
+        skills_certifications: "Certifié en design graphique utilisant Adobe Illustrator ainsi que la maîtrise de divers autres outils de design graphique et d'édition vidéo.<br><br><hr><br>Certifié en MS Excel; avec une connaissance appropriée de l'utilisation de la suite MS Office.",
+        skills_drawing: "Haute compétence en dessin au crayon ou numérique avec une connaissance moyenne sur l'animation",
+
+        //Projects Page
+        projects_title: "Mes Projets",
+        project1_desc: "Système de gestion d'événements utilisant HTML, CSS et JavaScript",
+        project2_desc: "Système de vote en Java",
+        project3_desc: "Copie du magasin E-Shop d'Amazon, avec JavaScript",
+        project4_desc: "Conseiller de modèles de cycle de vie du développement logiciel",
+        project5_desc: "Page d'atterrissage pour stages",
+        project6_desc: "Système de gestion des emplois du temps et de cartographie sur le campus",
+
+
+        //Contact Page
+        contact_me: "Contactez-moi"
+    }
+};
